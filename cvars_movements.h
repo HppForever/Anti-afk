91d026bf55e148c8742552a098bbab55bccb97e3
Миша <125406303+HppForever@users.noreply.github.com>
2024-04-06 22:40:18 +0300

    Add files via upload

---
+++ cvars_movements.h
@@ -0,0 +1,15 @@
+struct movement
+{
+/// etc cvars
+
+    struct anti_afk
+	{
+		bool    enable;
+		bool    auto_disable_on_activity;
+		int     attack_type;
+		int     rotation_type;
+		int     direction;
+		int     position;
+		float   interval;
+	} anti_afk;
+}
